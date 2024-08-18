25b324dcbbc83d20a440717b590b15309bfbd9c2
Tow Boon Ng <ng_tow_boon@aiap.sg>
2024-08-18 23:23:26 +0800

    feat: Added FewShotLLM PSCInspector and Prompt Templates. #6

--- src/psclabeler/model/labeler.py
+++ src/psclabeler/model/labeler.py
@@ -5,6 +5,8 @@ import os
 from abc import ABC, abstractmethod
 
 from langchain_core.messages.base import BaseMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts.few_shot import FewShotPromptTemplate
 from langchain_openai import AzureChatOpenAI
 
 import psclabeler as psc
@@ -70,5 +72,78 @@ class ZeroShotLLMPSCInspector(PSCInspector):
         response = self.model.invoke(
             input=[self.sys_prompt, context_prompt], temperature=0
         )
+
+        return response
+
+
+class FewShotLLMPSCInspector(PSCInspector):
+    """PSC Inspector that uses LLM with few shot prompting."""
+
+    def __init__(self) -> None:
+        """Initialize PSCInspector with LLM."""
+        if not os.getenv("AZURE_OPENAI_API_KEY"):
+            logger.error("No API key detected in environment variable.")
+            raise ValueError(
+                "No API key found. Please set your AZURE_OPENAI_API_KEY in the .env file."
+            )
+
+        self.model = AzureChatOpenAI(
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+            api_version=os.getenv("AZURE_API_VERSION"),
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            deployment_name=os.getenv("AZURE_MODEL"),
+            temperature=0,
+        )
+
+    @property
+    def few_shot_example_prompt(self):
+        """Define system prompt for model."""
+        self._example_prompt = psc.model.prompt.FEW_SHOT_EXAMPLES
+        return self._example_prompt
+
+    @property
+    def risk_guideline(self):
+        """Define risk guideline prompt for model."""
+        self._risk_guideline = psc.model.prompt.RISK_ASSESSMENT
+        return self._risk_guideline
+
+    @property
+    def few_shot_prompt_suffix(self):
+        """Define suffix questions for few shot prompt."""
+        self._few_shot_suffix = psc.model.prompt.FEW_SHOT_SUFFIX
+        return self._few_shot_suffix
+
+    def _create_few_shot_template(self) -> FewShotPromptTemplate:
+
+        example_prompt = PromptTemplate(
+            input_variables=["deficiency", "response"],
+            template="{deficiency}\n{response}",
+        )
+
+        prompt = FewShotPromptTemplate(
+            prefix=psc.model.prompt.PREFIX_PROMPT.format(
+                risk_assessment=self.risk_guideline
+            ),
+            examples=self.few_shot_example_prompt,
+            example_prompt=example_prompt,
+            suffix=self.few_shot_prompt_suffix,
+            input_variables=["input"],
+        )
+
+        return prompt
+
+    def rate_risk(self, deficiency_query: str) -> BaseMessage:
+        """Main method to rate risk for PSC report.
+
+        Args:
+            deficiency_query (str): A single PSC Report
+
+        Returns:
+            BaseMessage: Contains Messages of the inputs and outputs of ChatModels.
+                response.content is the string content of the message.
+        """
+        self.prompt = self._create_few_shot_template()
+        chain = self.prompt | self.model
+        response = chain.invoke(input=deficiency_query)
 
         return response
--- src/psclabeler/model/prompt.py
+++ src/psclabeler/model/prompt.py
@@ -47,3 +47,121 @@ ZERO_SHOT_PROMPT = PromptTemplate.from_template(
     Classification: Only reply one word. Do not explain further
     """
 )
+
+PREFIX_PROMPT = PromptTemplate.from_template(
+    """ 
+    Given the following risk assessment guidelines: 
+    {risk_assessment}
+    ---
+
+    Analyse the deficiency and root cause below step by step
+    And classify the deficiency accordingly to the risk level.
+    ---
+
+    Give your response in the following format:
+    Deficiency: What is it.
+
+    Reason: 
+    Analyze the Deficiency Step by Step before classification.
+    Does it expose a weakness in organization's processes?
+    Is it significant and potential threat to human life and cause accident or fire?
+
+
+    Classification: Only reply one word. Do not explain further
+    """
+)
+
+FEW_SHOT_EXAMPLES = [
+    {
+        "deficiency": """
+        Deficiency: Fire extinguisher for rescue boat rusted seriously.
+        Root cause: Inappropriate storage Fire extinguisher was not protected from weather.
+        Corrective action: Fire extinguisher replaced with a new extinguisher. The extinguisher is kept covered 
+        for protection against weather.
+        Preventive Action: Brieifing of entire ship staff carried out by Superintendent as to checks of rescue boat 
+        equipment. Lessons learned shared with all the vessels in Fleet.
+
+        Give your response in the following format:
+        Deficiency: What is it.
+
+        Reason: 
+        Analyze the Deficiency Step by Step before classification.
+        Does it expose a weakness in organization's processes?
+        Is it significant and potential threat to human life and cause accident or fire?
+
+
+        Classification: Only reply one word. Do not explain further
+        """,
+        "response": """
+        deficiency: Fire extinguisher for rescue boat rusted seriously.
+
+        reason: This is a potential threat to human life and shipboard accident if a fire breaks down, and the extinguisher is not usable.
+        Inapproriate storage of fire extinguisher is also a lapse in management procedure.
+
+        clssification: High Risk.
+""",
+    },
+    {
+        "deficiency": """
+        Deficiency: The company name on the DOC is not the same as on the CSR. The interim SMC and interim Security certificate have different company names to the DOC.
+        Root cause: Company stated in CSR doc not same as the DOC. Master without fail to cross check trading certificate.
+        Corrective action: Master w/o fail to cross check all trading certificates and to report Office in case of discrepancy. Shore base also to support to see to it all certificate issued in accordance of the requirement.
+        Preventive action: Education (Education and Training) Briefing of master carried out for checking and keeping up to date all trading certificates.
+
+        Give your response in the following format:
+        Deficiency: What is it.
+
+        Reason: 
+        Analyze the Deficiency Step by Step before classification.
+        Does it expose a weakness in organization's processes?
+        Is it significant and potential threat to human life and cause accident or fire?
+
+
+        Classification: Only reply one word. Do not explain further
+        """,
+        "response": """
+        deficiency: The company name on the DOC is not the same as on the CSR. The interim SMC and interim Security certificate have different company names to the DOC.
+
+        reason: Improper certification is a weakness in the organization to maintain proper documentation. There is no immediate threath to human life or accident.
+
+        clssification: Medium Risk.
+""",
+    },
+    {
+        "deficiency": """
+        Deficiency: Third mate was forgotten to record fire and abandon drill in Nav Logbook.
+        Root cause: Third mate was not familiar with special requirements of Nav logbook regarding drill record, 
+        which issued by CHINA MSA.
+        Corrective action: 1. Add abandon and fire drill in Nav Logbook at major event column.
+        2. Training all crew regarding special requirement regarding drill record in Nav Logbook.
+        Preventive action: 1. All drill record should be recorded in nav logbook after drill all complete.
+        2. Master should supervise whether duty officer was recording properly.
+
+        Give your response in the following format:
+        Deficiency: What is it.
+
+        Reason: 
+        Analyze the Deficiency Step by Step before classification.
+        Does it expose a weakness in organization's processes?
+        Is it significant and potential threat to human life and cause accident or fire?
+
+
+        Classification: Only reply one word. Do not explain further
+        """,
+        "response": """
+        deficiency: Third mate was forgotten to record fire and abandon drill in Nav Logbook.
+
+        reason: Third mate forgotten to record due to unfamiliarity of recording procedure. This is a lapse in adminstration but the drill is completed.
+        There is no threat to human life or potential accident. 
+
+        clssification: Low Risk.
+""",
+    },
+]
+
+FEW_SHOT_SUFFIX = """
+Analyse the deficiency and root cause below step by step
+And classify the deficiency accordingly to the risk level, High, Medium Low.
+
+Deficiency: {input}
+"""
